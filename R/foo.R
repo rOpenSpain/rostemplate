62e03d742bf7918093dea79b54cdb821383e952b
dieghernan <diego.hernangomezherrero@gmail.com>
2020-12-06 22:09:02 +0100

    Init

---
+++ R/foo.R
@@ -0,0 +1,11 @@
+#' Example function
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+foo <- function(x, y) {
+  x + y
+}
