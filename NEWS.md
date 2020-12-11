371c2bf4854791678356c033c15ab8b4f25ec340
dieghernan <diego.hernangomezherrero@gmail.com>
2020-12-11 17:34:41 +0100

    Go

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+
+# pkgdowntemp 0.0.0.91
+
+* Test
+
+
+
+# pkgdowntemp 0.0.0.90
+
+* Test
--- docs/ropenspain.js
+++ docs/ropenspain.js
@@ -1,12 +1,6 @@
 var h1s = document.getElementsByTagName("h1");
-
-console.log(h1s);
 var myH1;
 if (h1s.length > 0) {
   myH1 = h1s[0];
-  myH1.setAttribute("style", "color: red;");
-  
-  
-} else {
-  // Trigger error
-}
+  myH1.classList.add("ROSh1");
+} 
